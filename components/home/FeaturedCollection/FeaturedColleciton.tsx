b504304e35c3cd9bd8c1a3ffecea71cf68a0a2a6
Christian Diaz Portela <christiandp7@gmail.com>
2021-08-15 21:09:18 -0400

    :lipstick: Add hover card to Featured collection

--- components/home/FeaturedCollection/FeaturedColleciton.tsx
+++ components/home/FeaturedCollection/FeaturedColleciton.tsx
@@ -23,7 +23,35 @@ const FeaturedRow: FC = () => {
         </div>
         <LogoIntimo className={s.logoIntimo} />
         <TextShopTimelessDuck className={s.textDuck} />
-        <div className={s.content}>content</div>
+        <div className={s.content}>
+          <div className={s.contentBody}>
+            <p>
+              SS21: <b>Timeless Duck Shirt: [Intimo y Personal]</b> belongs to the
+              line with most recognition of LEBLANCSTUDIOS. For this season, we
+              explored the private sphere of our clients using clothes that reflect
+              the Caribbean masculinity and femininity. Creating a character based on
+              traditional clothing typologies of our country and interpreting the
+              sillhouettes and colors that typically define Caribbean casualwear in a
+              new way. We expanded our range of products, developing denim shirts,
+              skirts bucket hats, tote bags, and more featured products. With patches
+              and embroidery motifs with messages like “Federación de Mujeres
+              Dominicanas 1962”, “Federación de Mujeres Puertorriquenas 1975”,
+              “African-Caribbean-Pacific”, that explore and encourage cultural
+              dialogues within the Caribbean region and the rest of the world.
+            </p>
+
+            <p>
+              The names of the pieces came from important names of overlooked people
+              in fashion. For example, Viola Longacre, the woman who had the oldest
+              pair of jeans.
+            </p>
+          </div>
+
+          <div className={s.contentfooter}>
+            The collection was developed in Santo Domingo, Dominican Republic and
+            produced in Peru, Turkey, China, and the Dominican Republic in 2021.
+          </div>
+        </div>
       </div>
       <div className={s.column}>
         <div className={s.imgContainer}>
