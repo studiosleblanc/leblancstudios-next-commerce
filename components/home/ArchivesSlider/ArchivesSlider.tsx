daec3bf8f76a9f11fd64f27bc10aa97be5b53d3b
Christian Diaz Portela <christiandp7@gmail.com>
2021-08-16 20:39:25 -0400

    :lipstick: Archives slider done!

--- components/home/ArchivesSlider/ArchivesSlider.tsx
+++ components/home/ArchivesSlider/ArchivesSlider.tsx
@@ -1,66 +1,71 @@
 import React, { FC, useEffect } from 'react'
 import Image from 'next/image'
-import { useKeenSlider } from 'keen-slider/react'
+import s from './ArchivesSlider.module.css'
+// import { useKeenSlider } from 'keen-slider/react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import SwiperCore, { Autoplay, EffectFade } from 'swiper/core'
 
-// interface Keen {
-//   sliderRef: HTMLDivElement
-//   slider: KeenSlider
-// }
+// install Swiper modules
+SwiperCore.use([Autoplay, EffectFade])
 
 const ArchivesSlider: FC = () => {
-  const [pause, setPause] = React.useState(false)
-  const [sliderRef, slider] = useKeenSlider<HTMLDivElement>({
-    loop: true,
-    duration: 1000,
-    dragStart: () => {
-      setPause(true)
-    },
-    dragEnd: () => {
-      setPause(false)
-    },
-  })
-
-  useEffect(() => {
-    const timer = setInterval(() => {
-      if (!pause && slider) {
-        slider.next()
-      }
-    }, 4500)
-    return () => {
-      clearInterval(timer)
-    }
-  }, [pause, slider])
-
   return (
-    <div ref={sliderRef} className="keen-slider">
-      <div className="keen-slider__slide number-slide1">
-        <Image
-          quality={85}
-          src="/assets/home/archive-1.jpg"
-          layout="responsive"
-          width={1920}
-          height={1360}
-        />
-      </div>
-      <div className="keen-slider__slide number-slide2">
-        <Image
-          quality={85}
-          src="/assets/home/archive-2.jpg"
-          layout="responsive"
-          width={1920}
-          height={1360}
-        />
-      </div>
-      <div className="keen-slider__slide number-slide3">
-        <Image
-          quality={85}
-          src="/assets/home/archive-3.jpg"
-          layout="responsive"
-          width={1920}
-          height={1360}
-        />
-      </div>
-    </div>
+    <>
+      <Swiper
+        // {...props}
+        // grabCursor
+        // pauseonmouseenter={false}
+        // breakpoints={breakpoints}
+        effect={'fade'}
+        loop
+        autoplay={{ delay: 3500 }}>
+        <SwiperSlide className={s.swiperSlide}>
+          <Image
+            quality={85}
+            src="/assets/home/archive-1.jpg"
+            layout="responsive"
+            width={1920}
+            height={1360}
+          />
+          <div className={s.slideCaption}>
+            <h5>
+              From Archivo General De La Nación, 1973. [Gregorio García Castro.]
+            </h5>
+            <h5>Fondo Editora Hoy</h5>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className={s.swiperSlide}>
+          <Image
+            quality={85}
+            src="/assets/home/archive-2.jpg"
+            layout="responsive"
+            width={1920}
+            height={1360}
+          />
+          <div className={s.slideCaption}>
+            <h5>
+              From Archivo General De La Nación, 1975. [Ornalndo Martínez Howley.]
+            </h5>
+            <h5>Fondo Editora Hoy</h5>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className={s.swiperSlide}>
+          <Image
+            quality={85}
+            src="/assets/home/archive-3.jpg"
+            layout="responsive"
+            width={1920}
+            height={1360}
+          />
+          <div className={s.slideCaption}>
+            <h5>
+              From Archivo General De La Nación, 1975. [Ornalndo Martínez Howley.]
+            </h5>
+            <h5>Fondo Editora Hoy</h5>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </>
   )
 }
 
