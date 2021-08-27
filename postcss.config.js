e7446d9b949707f3c3b70bb50eaafc5b2d466e54
Christian Diaz Portela <christiandp7@gmail.com>
2021-08-27 16:52:15 -0400

    :wrench: Enable JIT and fix @apply nested rules out of @screen rules

--- postcss.config.js
+++ postcss.config.js
@@ -1,20 +1,29 @@
+// module.exports = {
+//   plugins: [
+//     // 'postcss-import',
+//     'tailwindcss',
+//     'postcss-nesting',
+//     'postcss-flexbugs-fixes',
+//     [
+//       'postcss-preset-env',
+//       {
+//         autoprefixer: {
+//           flexbox: 'no-2009',
+//         },
+//         stage: 3,
+//         features: {
+//           'custom-properties': false,
+//         },
+//       },
+//     ],
+//   ],
+// }
+
 module.exports = {
   plugins: [
-    // 'postcss-import',
     'tailwindcss',
     'postcss-nesting',
     'postcss-flexbugs-fixes',
-    [
-      'postcss-preset-env',
-      {
-        autoprefixer: {
-          flexbox: 'no-2009',
-        },
-        stage: 3,
-        features: {
-          'custom-properties': true,
-        },
-      },
-    ],
+    'autoprefixer',
   ],
 }
