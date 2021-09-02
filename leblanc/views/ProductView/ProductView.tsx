e92f2bab31f2d2ffe89f8a89d8ab90be0c70475a
Christian Diaz Portela <christiandp7@gmail.com>
2021-09-01 22:37:00 -0400

    :sparkles: Description row added on product

--- leblanc/views/ProductView/ProductView.tsx
+++ leblanc/views/ProductView/ProductView.tsx
@@ -25,7 +25,20 @@ const ProductView: FC<Props> = ({ product, relatedProducts }) => {
         </div>
       </div>
       <ProductSlider />
-      <div>description row</div>
+      <div className={s.descriptionRow}>
+        <h2>Black World Mass Observers T-Shirt</h2>
+        <p>
+          World Mass Observers t-shirt belongs to our latest collection No Medium
+          Inserted F/W 2022, presented at the Dominican Republic. It features and
+          reassambles the story of LEBLANCSTUDIOS a movement founded by two kids in
+          the 2014. The Mass-Observation project of United Kingdom which conducted a
+          series of human behaviour at work, on the street and many public spaces. We
+          use this graphic as an analogy of how nowadays we are being watched and
+          tracked by big corporations, same as The Mass-Observation Project did in
+          the 1937-1960 with paid investigators and anonymously recording
+          conversations.
+        </p>
+      </div>
       <div>related products row</div>
     </MainLayout>
   )
