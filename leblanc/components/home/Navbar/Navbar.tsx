6d066292fb172ca02a09411c9b0b021578e2ba98
Christian Diaz Portela <christiandp7@gmail.com>
2021-09-10 00:57:11 -0400

    :lipstick: Navbar ready to collapse

--- leblanc/components/home/Navbar/Navbar.tsx
+++ leblanc/components/home/Navbar/Navbar.tsx
@@ -1,8 +1,19 @@
+import React, { FC, useState, useEffect } from 'react'
 import { SearchIcon } from '@leblanc/icons'
-import React, { FC } from 'react'
 import s from './Navbar.module.css'
 
 const Navbar: FC = () => {
+  const [activeChild, setActiveChild] = useState('')
+
+  const handleActiveChild = (event: React.MouseEvent, target: string) => {
+    event.preventDefault()
+    setActiveChild(target)
+  }
+
+  useEffect(() => {
+    console.log(activeChild)
+  }, [activeChild])
+
   return (
     <div className={s.root}>
       <div className={s.navbar}>
@@ -10,19 +21,29 @@ const Navbar: FC = () => {
           <div className={s.toolbar}>
             <ul className={s.menu}>
               <li>
-                <a href="">New Arrivals</a>
+                <a href="#" onClick={e => handleActiveChild(e, 'tab-1')}>
+                  New Arrivals
+                </a>
               </li>
               <li>
-                <a href="">Womens</a>
+                <a href="#" onClick={e => handleActiveChild(e, 'tab-2')}>
+                  Womens
+                </a>
               </li>
               <li>
-                <a href="">Mens</a>
+                <a href="#" onClick={e => handleActiveChild(e, 'tab-3')}>
+                  Mens
+                </a>
               </li>
               <li>
-                <a href="">History</a>
+                <a href="#" onClick={e => handleActiveChild(e, 'tab-4')}>
+                  History
+                </a>
               </li>
               <li>
-                <a href="">Archives</a>
+                <a href="#" onClick={e => handleActiveChild(e, 'tab-5')}>
+                  Archives
+                </a>
               </li>
             </ul>
             <div className={s.search}>
@@ -42,8 +63,14 @@ const Navbar: FC = () => {
               </form>
             </div>
           </div>
+          <div className={s.collapseTop}>1</div>
         </div>
-        <div className={s.bottomRow}></div>
+        <div className={s.bottomRow}>
+          {/* {
+            activeChild !== '' && (
+            )
+          } */}
+        </div>
       </div>
     </div>
   )
