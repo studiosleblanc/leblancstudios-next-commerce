d0e68e5e37dfea25dd15f58dc9ac4ce604edbc7d
Christian Diaz Portela <christiandp7@gmail.com>
2021-10-04 08:21:11 -0400

    :sparkles: Add progress loader

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,7 @@ import type { AppProps } from 'next/app'
 import { Head } from '@components/common'
 import { ManagedUIContext } from '@components/ui/context'
 import { ThemeProvider } from 'next-themes'
+import NextNprogress from 'nextjs-progressbar'
 
 const Noop: FC = ({ children }) => <>{children}</>
 
@@ -26,6 +27,12 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           attribute="class"
           forcedTheme={(Component as any).theme || null}>
           <Layout pageProps={pageProps}>
+            <NextNprogress
+              color="#000"
+              startPosition={0.3}
+              stopDelayMs={500}
+              height={2}
+            />
             <Component {...pageProps} />
           </Layout>
         </ThemeProvider>
