eac7e46b8ea714903d90a05092939b1fb25dce40
Christian Diaz Portela <christiandp7@gmail.com>
2021-11-15 01:32:44 -0400

    :sparkles: Add New Arrivals > Mens link

--- leblanc/data/navigation/newArrivals/na_mens.tsx
+++ leblanc/data/navigation/newArrivals/na_mens.tsx
@@ -5,42 +5,56 @@ const mens: NavItem[] = [
     label: 'All',
     handle: 'all',
     href: '/new-arrivals/mens/all',
+    slug: 'new-arrivals-mens-all'
   },
   {
     id: 'shirts-56s54s',
     label: 'Shirts',
     handle: 'shirts',
     href: '/new-arrivals/mens/shirts',
+    slug: 'new-arrivals-mens-shirts'
   },
   {
     id: 't-Shirt-656d45s',
     label: 'T-Shirt',
     handle: 't-shirt',
     href: '/new-arrivals/mens/t-shirt',
+    slug: 'new-arrivals-mens-t-shirts'
   },
   {
     id: 'trousers-g5654s',
     label: 'Trousers',
     handle: 'trousers',
     href: '/new-arrivals/mens/trousers',
+    slug: 'new-arrivals-mens-trousers'
   },
   {
     id: 'accesories-67d5sd',
     label: 'Accesories',
     handle: 'accesories',
     href: '/new-arrivals/mens/accesories',
+    slug: 'new-arrivals-mens-accessories'
+  },
+  {
+    id: 'hoodies-456eGsd',
+    label: 'Hoodies',
+    handle: 'hoodies',
+    href: '/new-arrivals/mens/hoodies',
+    slug: 'new-arrivals-mens-hoodies'
   },
   {
     id: 'caps-675d54',
     label: 'Caps',
     handle: 'caps',
     href: '/new-arrivals/mens/caps',
+    slug: 'new-arrivals-mens-caps'
   },
   {
     id: 'leather-Goods-n78g6f765',
     label: 'Leather Goods',
     handle: 'leather-goods',
     href: '/new-arrivals/mens/leather-goods',
+    slug: 'new-arrivals-mens-leather-goods'
   },
 ]
 
