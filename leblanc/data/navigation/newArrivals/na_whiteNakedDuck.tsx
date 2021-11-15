5d28cc5e184531a236603cf59a57a9e8bbf98382
Christian Diaz Portela <christiandp7@gmail.com>
2021-11-15 01:46:01 -0400

    :sparkles: Connect new Arrivals > White Naked Duck links

--- leblanc/data/navigation/newArrivals/na_whiteNakedDuck.tsx
+++ leblanc/data/navigation/newArrivals/na_whiteNakedDuck.tsx
@@ -6,18 +6,21 @@ const whiteNakedDuck: NavItem[] = [
     label: 'Shirts',
     handle: 'shirts',
     href: '/new-arrivals/white-naked-duck/shirts',
+    slug: 'new-arrivals-white-naked-duck-shirts'
   },
   {
     id: 't-Shirt-m098h879h9',
     label: 'T-Shirt',
     handle: 't-shirt',
     href: '/new-arrivals/white-naked-duck/t-shirt',
+    slug: 'new-arrivals-white-naked-duck-t-shirts'
   },
   {
     id: 'hoodies-7g87r6',
     label: 'Hoodies',
     handle: 'hoodies',
     href: '/new-arrivals/white-naked-duck/hoodies',
+    slug: 'new-arrivals-white-naked-duck-hoodies'
   },
 ]
 
