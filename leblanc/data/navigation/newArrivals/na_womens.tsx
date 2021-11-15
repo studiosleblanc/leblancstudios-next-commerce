6fe93b359354e9dd87692c0184b47d1a197b17db
Christian Diaz Portela <christiandp7@gmail.com>
2021-11-15 01:28:22 -0400

    :sparkles: Add New Arrivals > Womens links

--- leblanc/data/navigation/newArrivals/na_womens.tsx
+++ leblanc/data/navigation/newArrivals/na_womens.tsx
@@ -6,48 +6,56 @@ const womens: NavItem[] = [
     label: 'All',
     handle: 'all',
     href: '/new-arrivals/womens/all',
+    slug: 'new-arrivals-womens-all'
   },
   {
     id: 'shirts-dsadaA',
     label: 'Shirts',
     handle: 'shirts',
     href: '/new-arrivals/womens/shirts',
+    slug: 'new-arrivals-womens-shirts'
   },
   {
     id: 't-Shirt-DJHYTDFJYH',
     label: 'T-Shirt',
     handle: 't-shirt',
     href: '/new-arrivals/womens/t-shirt',
+    slug: 'new-arrivals-womens-t-shirts'
   },
   {
     id: 'dresses-LYFYUFT',
     label: 'Dresses',
     handle: 'dresses',
     href: '/new-arrivals/womens/dresses',
+    slug: 'new-arrivals-womens-dresses'
   },
   {
     id: 'trousers-yg786t87g8',
     label: 'Trousers',
     handle: 'trousers',
     href: '/new-arrivals/womens/trousers',
+    slug: 'new-arrivals-womens-trousers'
   },
   {
     id: 'accesories-yku45d',
     label: 'Accesories',
     handle: 'accesories',
     href: '/new-arrivals/womens/accesories',
+    slug: 'new-arrivals-womens-accessories'
   },
   {
     id: 'caps-67fu56d',
     label: 'Caps',
     handle: 'caps',
     href: '/new-arrivals/womens/caps',
+    slug: 'new-arrivals-womens-caps'
   },
   {
     id: 'leather-Goods-ytf5464354',
     label: 'Leather Goods',
     handle: 'leather-goods',
     href: '/new-arrivals/womens/leather-goods',
+    slug: 'new-arrivals-womens-leather-goods'
   },
 ]
 
