59495d5d7e5e5aa5b9efc8f85c905d22376a7e59
Christian Diaz Portela <christiandp7@gmail.com>
2021-12-22 23:39:58 -0400

    :bug: Fix responsive featured section Home, Fix product item caption on Collection

--- tailwind.config.js
+++ tailwind.config.js
@@ -92,12 +92,13 @@ module.exports = {
         },
       },
     },
-    screen: {
+    screens: {
       sm: '640px',
       md: '768px',
       lg: '1024px',
       xl: '1280px',
       '2xl': '1536px',
+      '3xl': '1736px',
     },
   },
 }
