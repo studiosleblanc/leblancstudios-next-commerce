4fb3ad986fa76a2e9fbe68cc1614fed7670489e1
Christian Diaz Portela <christiandp7@gmail.com>
2021-12-24 18:13:06 -0400

    :lipstick: Home: Remove unnecessary sections in Mobile

--- leblanc/components/home/ArchivesSlider/ArchivesSlider.tsx
+++ leblanc/components/home/ArchivesSlider/ArchivesSlider.tsx
@@ -5,6 +5,7 @@ import { Swiper, SwiperSlide } from 'swiper/react'
 import SwiperCore, { Autoplay, EffectFade } from 'swiper/core'
 import { useInView } from 'react-intersection-observer'
 import { motion } from 'framer-motion'
+import { useMediaQuery } from 'react-responsive'
 import { TextArchives } from '@leblanc/svg'
 
 // install Swiper modules
@@ -13,6 +14,8 @@ SwiperCore.use([Autoplay, EffectFade])
 const ArchivesSlider: FC = () => {
   const [slide, setSlide] = useState(false)
   const [ref, inView] = useInView({ triggerOnce: true })
+  const isDesktop = useMediaQuery({ query: '(min-width: 1024px)' })
+
   const sliderSvgAnimate = {
     enter: {
       opacity: 1,
@@ -37,67 +40,69 @@ const ArchivesSlider: FC = () => {
   }
   return (
     <>
-      <Swiper effect={'fade'} loop autoplay={{ delay: 4000 }}>
-        <SwiperSlide className={s.swiperSlide}>
-          {({ isActive }) => {
-            return (
-              <>
-                <Image
-                  unoptimized
-                  src="/assets/home/archive-1.jpg"
-                  layout="responsive"
-                  width={1920}
-                  height={1360}
-                />
-                <motion.div
-                  className={s.slideSvgWrapper}
-                  initial="exit"
-                  animate={isActive ? 'enter' : 'exit'}
-                  variants={sliderSvgAnimate}>
-                  <TextArchives className={s.slideSvgText} />
-                </motion.div>
-                <div className={s.slideCaption}>
-                  <h5>
-                    From Archivo General De La Nación, 1973. [Gregorio García
-                    Castro.]
-                  </h5>
-                  <h5>Fondo Editora Hoy</h5>
-                </div>
-              </>
-            )
-          }}
-        </SwiperSlide>
-        <SwiperSlide className={s.swiperSlide}>
-          <Image
-            unoptimized
-            src="/assets/home/archive-2.jpg"
-            layout="responsive"
-            width={1920}
-            height={1360}
-          />
-          <div className={s.slideCaption}>
-            <h5>
-              From Archivo General De La Nación, 1975. [Ornalndo Martínez Howley.]
-            </h5>
-            <h5>Fondo Editora Hoy</h5>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className={s.swiperSlide}>
-          <Image
-            unoptimized
-            src="/assets/home/archive-3.jpg"
-            layout="responsive"
-            width={1920}
-            height={1360}
-          />
-          <div className={s.slideCaption}>
-            <h5>
-              From Archivo General De La Nación, 1975. [Ornalndo Martínez Howley.]
-            </h5>
-            <h5>Fondo Editora Hoy</h5>
-          </div>
-        </SwiperSlide>
-      </Swiper>
+      {isDesktop && (
+        <Swiper effect={'fade'} loop autoplay={{ delay: 4000 }}>
+          <SwiperSlide className={s.swiperSlide}>
+            {({ isActive }) => {
+              return (
+                <>
+                  <Image
+                    unoptimized
+                    src="/assets/home/archive-1.jpg"
+                    layout="responsive"
+                    width={1920}
+                    height={1360}
+                  />
+                  <motion.div
+                    className={s.slideSvgWrapper}
+                    initial="exit"
+                    animate={isActive ? 'enter' : 'exit'}
+                    variants={sliderSvgAnimate}>
+                    <TextArchives className={s.slideSvgText} />
+                  </motion.div>
+                  <div className={s.slideCaption}>
+                    <h5>
+                      From Archivo General De La Nación, 1973. [Gregorio García
+                      Castro.]
+                    </h5>
+                    <h5>Fondo Editora Hoy</h5>
+                  </div>
+                </>
+              )
+            }}
+          </SwiperSlide>
+          <SwiperSlide className={s.swiperSlide}>
+            <Image
+              unoptimized
+              src="/assets/home/archive-2.jpg"
+              layout="responsive"
+              width={1920}
+              height={1360}
+            />
+            <div className={s.slideCaption}>
+              <h5>
+                From Archivo General De La Nación, 1975. [Ornalndo Martínez Howley.]
+              </h5>
+              <h5>Fondo Editora Hoy</h5>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide className={s.swiperSlide}>
+            <Image
+              unoptimized
+              src="/assets/home/archive-3.jpg"
+              layout="responsive"
+              width={1920}
+              height={1360}
+            />
+            <div className={s.slideCaption}>
+              <h5>
+                From Archivo General De La Nación, 1975. [Ornalndo Martínez Howley.]
+              </h5>
+              <h5>Fondo Editora Hoy</h5>
+            </div>
+          </SwiperSlide>
+        </Swiper>
+      )}
     </>
   )
 }
