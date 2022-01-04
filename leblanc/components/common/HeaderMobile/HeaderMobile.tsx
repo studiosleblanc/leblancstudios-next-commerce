ebbcebaa6e999ca9d4650657f8640520d14ab2b0
Christian Diaz Portela <christiandp7@gmail.com>
2022-01-04 00:10:33 -0400

    :lipstick: Style navbar on mobile

--- leblanc/components/common/HeaderMobile/HeaderMobile.tsx
+++ leblanc/components/common/HeaderMobile/HeaderMobile.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { HeaderLogos } from '..'
 import s from './HeaderMobile.module.css'
 
 const HeaderMobile = () => {
-  return <div>header Mobile</div>
+  return (
+    <div className={s.wrapper}>
+      <div className={s.root}>
+        <div className={s.leftCol}>
+          <HeaderLogos />
+        </div>
+        <div className={s.rightCol}>toolbar</div>
+      </div>
+    </div>
+  )
 }
 
 export default HeaderMobile
