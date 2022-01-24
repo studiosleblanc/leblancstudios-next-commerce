7c2a2baf69458c731c9fccb0ef697064561674e1
Christian Diaz Portela <christiandp7@gmail.com>
2022-01-23 20:33:46 -0400

    Add dynamic text as prop on Collection Prop

--- leblanc/components/collection/CollectionEmpty/CollectionEmpty.tsx
+++ leblanc/components/collection/CollectionEmpty/CollectionEmpty.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { FC } from 'react'
 import s from './CollectionEmpty.module.css'
 
-function CollectionEmpty() {
-  return <div className={s.root}><span>Collection Empty</span></div>
+interface Props {
+  text: string
+}
+
+const CollectionEmpty: FC<Props> = ({ text }) => {
+  return (
+    <div className={s.root}>
+      <span>{text}</span>
+    </div>
+  )
 }
 
 export default CollectionEmpty
