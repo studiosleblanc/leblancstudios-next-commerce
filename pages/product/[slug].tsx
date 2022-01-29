c04861ae77ac0f79d84eb51547dfaace96e571b3
Christian Diaz Portela <christiandp7@gmail.com>
2022-01-29 19:49:12 -0400

    :recycle: Clean code removing [slug]-old.tsx because of a warning

--- pages/product/[slug].tsx
+++ pages/product/[slug].tsx
@@ -10,6 +10,63 @@ import commerce from '@lib/api/commerce'
 import { Layout } from '@leblanc/layouts'
 import { ProductView } from '@leblanc/views'
 
+// export async function getStaticProps({
+//   params,
+//   locale,
+//   locales,
+//   preview,
+// }: GetStaticPropsContext<{ slug: string }>) {
+//   const config = { locale, locales }
+//   const pagesPromise = commerce.getAllPages({ config, preview })
+//   const siteInfoPromise = commerce.getSiteInfo({ config, preview })
+//   const productPromise = commerce.getProduct({
+//     variables: { slug: params!.slug },
+//     config,
+//     preview,
+//   })
+
+//   const allProductsPromise = commerce.getAllProducts({
+//     variables: { first: 4 },
+//     config,
+//     preview,
+//   })
+//   const { pages } = await pagesPromise
+//   const { categories } = await siteInfoPromise
+//   const { product } = await productPromise
+//   const { products: relatedProducts } = await allProductsPromise
+
+//   if (!product) {
+//     throw new Error(`Product with slug '${params!.slug}' not found`)
+//   }
+
+//   return {
+//     props: {
+//       pages,
+//       product,
+//       relatedProducts,
+//       categories,
+//     },
+//     revalidate: 200,
+//   }
+// }
+
+// export async function getStaticPaths({ locales }: GetStaticPathsContext) {
+//   const { products } = await commerce.getAllProductPaths()
+
+//   return {
+//     paths: locales
+//       ? locales.reduce<string[]>((arr, locale) => {
+//           // Add a product path for every locale
+//           products.forEach((product: any) => {
+//             arr.push(`/${locale}/product${product.path}`)
+//           })
+//           return arr
+//         }, [])
+//       : products.map((product: any) => `/product${product.path}`),
+//     fallback: 'blocking',
+//   }
+// }
+
 export async function getServerSideProps({
   params,
   locale,
@@ -48,23 +105,6 @@ export async function getServerSideProps({
     }
   }
 }
-
-// export async function getStaticPaths({ locales }: GetStaticPathsContext) {
-//   const { products } = await commerce.getAllProductPaths()
-
-//   return {
-//     paths: locales
-//       ? locales.reduce<string[]>((arr, locale) => {
-//           // Add a product path for every locale
-//           products.forEach((product: any) => {
-//             arr.push(`/${locale}/product${product.path}`)
-//           })
-//           return arr
-//         }, [])
-//       : products.map((product: any) => `/product${product.path}`),
-//     fallback: 'blocking',
-//   }
-// }
 
 export default function Slug({
   product,
