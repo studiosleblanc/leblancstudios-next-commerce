8f24fc9d21511cbbae1be6b84902e6bc6820d2ab
Christian Diaz Portela <christiandp7@gmail.com>
2022-01-29 19:07:51 -0400

    :building_construction: Change product page static incremental generation by SSR

--- pages/product/[slug]-old.tsx
+++ pages/product/[slug]-old.tsx
@@ -1,21 +1,19 @@
 import type {
-  // GetStaticPathsContext,
-  // GetStaticPropsContext,
-  // InferGetStaticPropsType,
-  GetServerSidePropsContext,
-  InferGetServerSidePropsType
+  GetStaticPathsContext,
+  GetStaticPropsContext,
+  InferGetStaticPropsType,
 } from 'next'
 import { useRouter } from 'next/router'
 import commerce from '@lib/api/commerce'
 import { Layout } from '@leblanc/layouts'
 import { ProductView } from '@leblanc/views'
 
-export async function getServerSideProps({
+export async function getStaticProps({
   params,
   locale,
   locales,
   preview,
-}: GetServerSidePropsContext<{ slug: string }>) {
+}: GetStaticPropsContext<{ slug: string }>) {
   const config = { locale, locales }
   const pagesPromise = commerce.getAllPages({ config, preview })
   const siteInfoPromise = commerce.getSiteInfo({ config, preview })
@@ -45,31 +43,32 @@ export async function getServerSideProps({
       product,
       relatedProducts,
       categories,
-    }
+    },
+    revalidate: 200,
   }
 }
 
-// export async function getStaticPaths({ locales }: GetStaticPathsContext) {
-//   const { products } = await commerce.getAllProductPaths()
+export async function getStaticPaths({ locales }: GetStaticPathsContext) {
+  const { products } = await commerce.getAllProductPaths()
 
-//   return {
-//     paths: locales
-//       ? locales.reduce<string[]>((arr, locale) => {
-//           // Add a product path for every locale
-//           products.forEach((product: any) => {
-//             arr.push(`/${locale}/product${product.path}`)
-//           })
-//           return arr
-//         }, [])
-//       : products.map((product: any) => `/product${product.path}`),
-//     fallback: 'blocking',
-//   }
-// }
+  return {
+    paths: locales
+      ? locales.reduce<string[]>((arr, locale) => {
+          // Add a product path for every locale
+          products.forEach((product: any) => {
+            arr.push(`/${locale}/product${product.path}`)
+          })
+          return arr
+        }, [])
+      : products.map((product: any) => `/product${product.path}`),
+    fallback: 'blocking',
+  }
+}
 
 export default function Slug({
   product,
   relatedProducts,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   const router = useRouter()
 
   return router.isFallback ? (
