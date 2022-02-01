60bc22d73f77025aacaba78b2d6398736e87efd4
Christian Diaz Portela <christiandp7@gmail.com>
2022-02-01 11:25:53 -0400

    :sparkles: Make Men image clickable

--- leblanc/components/home/MenWomen/MenWomen.tsx
+++ leblanc/components/home/MenWomen/MenWomen.tsx
@@ -1,4 +1,5 @@
 import React, { FC, useState } from 'react'
+import Link from 'next/link'
 import cn from 'classnames'
 import Image from 'next/image'
 import s from './MenWomen.module.css'
@@ -10,48 +11,50 @@ const MenWomen: FC = () => {
   return (
     <div className={s.root}>
       <div className={s.column}>
-        <div className={s.card}>
-          <Image
-            priority
-            unoptimized
-            src="/assets/home/mens.jpg"
-            alt="Leblanc Studios"
-            width={963}
-            height={1443}
-            layout="responsive"
-            className={s.image}
-            placeholder="empty"
-            onLoadingComplete={() => setMenLoaded(true)}
-          />
-          <div className={s.cardContent}>
-            <div className={s.cardHeader}>
-              <div className={s.headerCaption}>
-                ss22: <b>[No Medium Inserted]</b>
+        <Link href="/product/black-lissajous-shirt">
+          <a className={s.card}>
+            <Image
+              priority
+              unoptimized
+              src="/assets/home/mens.jpg"
+              alt="Leblanc Studios"
+              width={963}
+              height={1443}
+              layout="responsive"
+              className={s.image}
+              placeholder="empty"
+              onLoadingComplete={() => setMenLoaded(true)}
+            />
+            <div className={s.cardContent}>
+              <div className={s.cardHeader}>
+                <div className={s.headerCaption}>
+                  ss22: <b>[No Medium Inserted]</b>
+                </div>
               </div>
-            </div>
-            <div className={s.cardFooter}>
-              <div className={s.footerCaption}>MENS</div>
-            </div>
+              <div className={s.cardFooter}>
+                <div className={s.footerCaption}>MENS</div>
+              </div>
 
-            <Tooltip
-              appear={menLoaded}
-              top="34%"
-              left="11%"
-              width="20.6%"
-              className={s.tooltip}
-              text="Shirt [NMI]"
-            />
-            <Tooltip
-              appear={menLoaded}
-              bottom="30%"
-              right="15%"
-              align="right"
-              width="23.5%"
-              className={s.tooltip}
-              text="Trousers [NMI]"
-            />
-          </div>
-        </div>
+              <Tooltip
+                appear={menLoaded}
+                top="34%"
+                left="11%"
+                width="20.6%"
+                className={s.tooltip}
+                text="Shirt [NMI]"
+              />
+              <Tooltip
+                appear={menLoaded}
+                bottom="30%"
+                right="15%"
+                align="right"
+                width="23.5%"
+                className={s.tooltip}
+                text="Trousers [NMI]"
+              />
+            </div>
+          </a>
+        </Link>
       </div>
       <div className={s.column}>
         <div className={s.card}>
