05191d3c25e2e9854dcaa3fb5e0e3a905c29cf87
Christian Diaz Portela <christiandp7@gmail.com>
2022-02-03 19:34:46 -0400

    :sparkles: Add collections paths

---
+++ leblanc/data/navigation/collections.tsx
@@ -0,0 +1,15 @@
+import type { NavItem } from '@leblanc/data/navigation'
+
+const collections: NavItem[] = [
+  {
+    id: 'tds-timeless-duck-shirt-intimo-y-personal-adwsaw78tf',
+    label: 'Timeless Duck Shirt: Intimo y Personal',
+    handle: 'all-tds',
+    abbr: 'tds',
+    href: '/collection/mens-tds',
+    slug: 'all-tds',
+    noNav: true,
+  },
+]
+
+export default collections
