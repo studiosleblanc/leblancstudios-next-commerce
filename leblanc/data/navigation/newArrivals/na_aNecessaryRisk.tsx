40a1d2dacd93084d2b78fb6e5d9e3bb55bf04518
Christian Diaz Portela <christiandp7@gmail.com>
2022-02-07 12:12:49 -0400

    :building_construction: Remove all ANR items from nav and left socks only

--- leblanc/data/navigation/newArrivals/na_aNecessaryRisk.tsx
+++ leblanc/data/navigation/newArrivals/na_aNecessaryRisk.tsx
@@ -1,34 +1,34 @@
 import type { NavItem } from '@leblanc/data/navigation'
 
 const aNecessaryRisk: NavItem[] = [
-  {
-    id: 'shirts-88g87rt65',
-    label: 'Shirts',
-    handle: 'shirts',
-    href: '/new-arrivals/a-necessary-risk-x-gus-pena/shirts',
-    slug: 'new-arrivals-a-necessary-risk-x-gus-pena-shirts'
-  },
-  {
-    id: 't-Shirt-908h78f',
-    label: 'T-Shirts',
-    handle: 't-shirts',
-    href: '/new-arrivals/a-necessary-risk-x-gus-pena/t-shirts',
-    slug: 'new-arrivals-a-necessary-risk-x-gus-pena-t-shirts'
-  },
-  {
-    id: 'trousers-h6d4',
-    label: 'Trousers',
-    handle: 'trousers',
-    href: '/new-arrivals/a-necessary-risk-x-gus-pena/trousers',
-    slug: 'new-arrivals-a-necessary-risk-x-gus-pena-trousers'
-  },
-  {
-    id: 'shorts-9n78g9o',
-    label: 'Shorts',
-    handle: 'shorts',
-    href: '/new-arrivals/a-necessary-risk-x-gus-pena/shorts',
-    slug: 'new-arrivals-a-necessary-risk-x-gus-pena-shorts'
-  },
+  // {
+  //   id: 'shirts-88g87rt65',
+  //   label: 'Shirts',
+  //   handle: 'shirts',
+  //   href: '/new-arrivals/a-necessary-risk-x-gus-pena/shirts',
+  //   slug: 'new-arrivals-a-necessary-risk-x-gus-pena-shirts'
+  // },
+  // {
+  //   id: 't-Shirt-908h78f',
+  //   label: 'T-Shirts',
+  //   handle: 't-shirts',
+  //   href: '/new-arrivals/a-necessary-risk-x-gus-pena/t-shirts',
+  //   slug: 'new-arrivals-a-necessary-risk-x-gus-pena-t-shirts'
+  // },
+  // {
+  //   id: 'trousers-h6d4',
+  //   label: 'Trousers',
+  //   handle: 'trousers',
+  //   href: '/new-arrivals/a-necessary-risk-x-gus-pena/trousers',
+  //   slug: 'new-arrivals-a-necessary-risk-x-gus-pena-trousers'
+  // },
+  // {
+  //   id: 'shorts-9n78g9o',
+  //   label: 'Shorts',
+  //   handle: 'shorts',
+  //   href: '/new-arrivals/a-necessary-risk-x-gus-pena/shorts',
+  //   slug: 'new-arrivals-a-necessary-risk-x-gus-pena-shorts'
+  // },
   {
     id: 'socks-435s56f7',
     label: 'Socks',
