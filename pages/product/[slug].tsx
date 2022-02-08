ce2ce9568b74212e67dd54cc4375007c8fe26cb5
Christian Diaz Portela <christiandp7@gmail.com>
2022-02-08 10:07:28 -0400

    :building_construction: Make product incremental static generation again

--- pages/product/[slug].tsx
+++ pages/product/[slug].tsx
@@ -1,78 +1,21 @@
 import type {
-  // GetStaticPathsContext,
-  // GetStaticPropsContext,
-  // InferGetStaticPropsType,
-  GetServerSidePropsContext,
-  InferGetServerSidePropsType
+  GetStaticPathsContext,
+  GetStaticPropsContext,
+  InferGetStaticPropsType,
+  // GetServerSidePropsContext,
+  // InferGetServerSidePropsType
 } from 'next'
 import { useRouter } from 'next/router'
 import commerce from '@lib/api/commerce'
 import { Layout } from '@leblanc/layouts'
 import { ProductView } from '@leblanc/views'
 
-// export async function getStaticProps({
-//   params,
-//   locale,
-//   locales,
-//   preview,
-// }: GetStaticPropsContext<{ slug: string }>) {
-//   const config = { locale, locales }
-//   const pagesPromise = commerce.getAllPages({ config, preview })
-//   const siteInfoPromise = commerce.getSiteInfo({ config, preview })
-//   const productPromise = commerce.getProduct({
-//     variables: { slug: params!.slug },
-//     config,
-//     preview,
-//   })
-
-//   const allProductsPromise = commerce.getAllProducts({
-//     variables: { first: 4 },
-//     config,
-//     preview,
-//   })
-//   const { pages } = await pagesPromise
-//   const { categories } = await siteInfoPromise
-//   const { product } = await productPromise
-//   const { products: relatedProducts } = await allProductsPromise
-
-//   if (!product) {
-//     throw new Error(`Product with slug '${params!.slug}' not found`)
-//   }
-
-//   return {
-//     props: {
-//       pages,
-//       product,
-//       relatedProducts,
-//       categories,
-//     },
-//     revalidate: 200,
-//   }
-// }
-
-// export async function getStaticPaths({ locales }: GetStaticPathsContext) {
-//   const { products } = await commerce.getAllProductPaths()
-
-//   return {
-//     paths: locales
-//       ? locales.reduce<string[]>((arr, locale) => {
-//           // Add a product path for every locale
-//           products.forEach((product: any) => {
-//             arr.push(`/${locale}/product${product.path}`)
-//           })
-//           return arr
-//         }, [])
-//       : products.map((product: any) => `/product${product.path}`),
-//     fallback: 'blocking',
-//   }
-// }
-
-export async function getServerSideProps({
+export async function getStaticProps({
   params,
   locale,
   locales,
   preview,
-}: GetServerSidePropsContext<{ slug: string }>) {
+}: GetStaticPropsContext<{ slug: string }>) {
   const config = { locale, locales }
   const pagesPromise = commerce.getAllPages({ config, preview })
   const siteInfoPromise = commerce.getSiteInfo({ config, preview })
@@ -102,14 +45,71 @@ export async function getServerSideProps({
       product,
       relatedProducts,
       categories,
-    }
+    },
+    revalidate: 200,
   }
 }
+
+export async function getStaticPaths({ locales }: GetStaticPathsContext) {
+  const { products } = await commerce.getAllProductPaths()
+
+  return {
+    paths: locales
+      ? locales.reduce<string[]>((arr, locale) => {
+          // Add a product path for every locale
+          products.forEach((product: any) => {
+            arr.push(`/${locale}/product${product.path}`)
+          })
+          return arr
+        }, [])
+      : products.map((product: any) => `/product${product.path}`),
+    fallback: 'blocking',
+  }
+}
+
+// export async function getServerSideProps({
+//   params,
+//   locale,
+//   locales,
+//   preview,
+// }: GetServerSidePropsContext<{ slug: string }>) {
+//   const config = { locale, locales }
+//   const pagesPromise = commerce.getAllPages({ config, preview })
+//   const siteInfoPromise = commerce.getSiteInfo({ config, preview })
+//   const productPromise = commerce.getProduct({
+//     variables: { slug: params!.slug },
+//     config,
+//     preview,
+//   })
+
+//   const allProductsPromise = commerce.getAllProducts({
+//     variables: { first: 4 },
+//     config,
+//     preview,
+//   })
+//   const { pages } = await pagesPromise
+//   const { categories } = await siteInfoPromise
+//   const { product } = await productPromise
+//   const { products: relatedProducts } = await allProductsPromise
+
+//   if (!product) {
+//     throw new Error(`Product with slug '${params!.slug}' not found`)
+//   }
+
+//   return {
+//     props: {
+//       pages,
+//       product,
+//       relatedProducts,
+//       categories,
+//     }
+//   }
+// }
 
 export default function Slug({
   product,
   relatedProducts,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   const router = useRouter()
 
   return router.isFallback ? (
