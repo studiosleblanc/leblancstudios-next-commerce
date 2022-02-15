0260154394ed66bebca1106adb0644aee65da68c
Christian Diaz Portela <christiandp7@gmail.com>
2022-02-15 14:45:17 -0400

    :monocle_face: Add data and images for A Necessary Ris, White Shirt 1 an 2

--- leblanc/data/archives/index.tsx
+++ leblanc/data/archives/index.tsx
@@ -28,10 +28,38 @@ export const archivesData: Archive[] = [
   {
     name: 'Fausse Geométrié',
     handle: 'fausse-geometrie',
+    richText:
+      'FAUSSE GEOMÉTRIÉ EMBRACES THE IMPERFECTION ABOUT THE CONSTRUCTION OF CLASSIC GARMENTS. DEVELOPING LOUNGE PANTS MADE OF CASHMERE, SWEATERS WITH PATCHES OF DISTORTED FIGURES, HAVING A RESULT OF A CASUAL AND FUN ENVIRONMENT.',
+    images: [
+      {
+        src: '/assets/archives/fausse-geometrie/fausse-geometrie-4.jpg',
+        width: 2400,
+        height: 1600,
+      },
+      {
+        src: '/assets/archives/fausse-geometrie/fausse-geometrie-2.jpg',
+        width: 2400,
+        height: 1600,
+      },
+      {
+        src: '/assets/archives/fausse-geometrie/fausse-geometrie-3.jpg',
+        width: 1600,
+        height: 2400,
+      },
+      {
+        src: '/assets/archives/fausse-geometrie/fausse-geometrie-1.jpg',
+        width: 2400,
+        height: 1600,
+      },
+    ],
   },
   {
     name: 'Kapput',
     handle: 'kapput',
+    richText:
+      'KAPUTT’S DISCOURSE COMPREHENDS THE HUMAN DISINTEGRATION IN MODERN HISTORY. IT’S BOUND FROM LINGUISTICS TO DISPOSABLE OBJECTS AND SELF-DESTRUCTIVE BEHAVIOUR... GENOCIDE, WHITE-COLLAR CRIMES, FAKE FRIENDSHIP, WAR AND PROPAGANDA. THE COLLECTION IS DEPICTED BY MILITARY SILHOUETTES, STRONG SHOULDER PADS, CARGO POCKETS SHIRT-JACKETS, PRINTS OF RELEVANT NEWS, ARCHIVE MATERIALS AND USELESS... COMPANY SLOGANS...',
+    credits:
+      'KAPUTT S/S 2016 WAS LAUNCHED AT THE CASA DE BASTIDAS MUSEUM AT THE ZONA COLONIAL, SANTO DOMINGO. AD CAMPAIGN SHOT BY MÁXIMO DEL CASTILLO.',
   },
   {
     name: 'Singular Unity',
@@ -63,10 +91,70 @@ export const archivesData: Archive[] = [
   {
     name: 'Foregn Affair / Criminal Intent, 1st. Edition',
     handle: 'white-shirt-17-1',
+    images: [
+      {
+        src: '/assets/archives/white-shirt-1/white-shirt_1_-1.jpg',
+        width: 3648,
+        height: 5472,
+      },
+      {
+        src: '/assets/archives/white-shirt-1/white-shirt_1_-2.jpg',
+        width: 5472,
+        height: 3648,
+      },
+      {
+        src: '/assets/archives/white-shirt-1/white-shirt_1_-3.jpg',
+        width: 3648,
+        height: 5472,
+      },
+      {
+        src: '/assets/archives/white-shirt-1/white-shirt_1_-4.jpg',
+        width: 3648,
+        height: 5472,
+      },
+      {
+        src: '/assets/archives/white-shirt-1/white-shirt_1_-5.jpg',
+        width: 5472,
+        height: 3648,
+      },
+      {
+        src: '/assets/archives/white-shirt-1/white-shirt_1_-6.jpg',
+        width: 5472,
+        height: 3648,
+      },
+    ],
+    credits:
+      'Photographed in the Dominican Republic at "Teatro de Agua y la Luz" by Luis Miguel Molina.',
   },
   {
     name: 'Identity Theft Cards, Bribery & Artnapping, 2nd. Edition',
     handle: 'white-shirt-17-2',
+    richText:
+      'WHITE SHIRT 2ND. EDITION LOOK INTO WHITE-COLLAR CRIMES, FROM IDENTITY THEFT CARDS, ODEBRECHT BRIBERY SCANDAL TO GLOBAL ARTNAPPING SITUATIONS. DELIVERED WITH STRONG GRAPHICS, INFORMATIVE LABELS, BEING THIS COLLECTION A DECISIVE INPUT IN OUR GRAPHIC AND COMMUNICATION IDENTITY, AND WHAT GOES AFTERWARDS.',
+    images: [
+      {
+        src: '/assets/archives/white-shirt-2/white-shirt_2_-1.jpg',
+        width: 1080,
+        height: 1920,
+      },
+      {
+        src: '/assets/archives/white-shirt-2/white-shirt_2_-2.jpg',
+        width: 1080,
+        height: 1920,
+      },
+      {
+        src: '/assets/archives/white-shirt-2/white-shirt_2_-3.jpg',
+        width: 1080,
+        height: 1920,
+      },
+      {
+        src: '/assets/archives/white-shirt-2/white-shirt_2_-4.jpg',
+        width: 1080,
+        height: 1920,
+      },
+    ],
+    credits:
+      "Photographed in the Dominican Republic at Adolfo's House. Photographed by Máximo del Castillo",
   },
   {
     name: 'Murder Is My Business, 3rd. Edition',
@@ -75,5 +163,60 @@ export const archivesData: Archive[] = [
   {
     name: 'Murder Is My Business II / El Pueblo Cuestiona, 4th. Edition',
     handle: 'white-shirt-19',
+  },
+  {
+    name: 'A Necessary Risk',
+    handle: 'a-necessary-risk',
+    images: [
+      {
+        src: '/assets/archives/a-necessary-risk/a-necessary-risk-1.jpg',
+        width: 1080,
+        height: 1619,
+      },
+      {
+        src: '/assets/archives/a-necessary-risk/a-necessary-risk-2.jpg',
+        width: 1080,
+        height: 1619,
+      },
+      {
+        src: '/assets/archives/a-necessary-risk/a-necessary-risk-3.jpg',
+        width: 1080,
+        height: 1619,
+      },
+      {
+        src: '/assets/archives/a-necessary-risk/a-necessary-risk-4.jpg',
+        width: 1080,
+        height: 1619,
+      },
+      {
+        src: '/assets/archives/a-necessary-risk/a-necessary-risk-5.jpg',
+        width: 1080,
+        height: 1619,
+      },
+      {
+        src: '/assets/archives/a-necessary-risk/a-necessary-risk-6.jpg',
+        width: 1080,
+        height: 1619,
+      },
+      {
+        src: '/assets/archives/a-necessary-risk/a-necessary-risk-7.jpg',
+        width: 1080,
+        height: 1619,
+      },
+      {
+        src: '/assets/archives/a-necessary-risk/a-necessary-risk-8.jpg',
+        width: 1151,
+        height: 768,
+      },
+      {
+        src: '/assets/archives/a-necessary-risk/a-necessary-risk-9.jpg',
+        width: 1151,
+        height: 768,
+      },
+    ],
+  },
+  {
+    name: 'No Medium Inserted',
+    handle: 'no-medium-inserted',
   },
 ]
--- leblanc/data/navigation/archives.tsx
+++ leblanc/data/navigation/archives.tsx
@@ -4,57 +4,115 @@ import type { NavItem } from '@leblanc/data/navigation'
 const archives: NavItem[] = [
   {
     id: 'la-cuisine',
-    label: <Fragment>SS14: <b>[La Cuisine, Everything Began With An Archive]</b></Fragment>,
+    label: (
+      <Fragment>
+        SS14: <b>[La Cuisine, Everything Began With An Archive]</b>
+      </Fragment>
+    ),
     handle: 'la-cuisine',
     href: '/archives/la-cuisine',
   },
   {
     id: 'timeless-duck-shirt-lolympique',
-    label: <Fragment>SS15: <b>[Timeless Duck Shirt: L’ Olympique]</b></Fragment>,
+    label: (
+      <Fragment>
+        SS15: <b>[Timeless Duck Shirt: L’ Olympique]</b>
+      </Fragment>
+    ),
     handle: 'timeless-duck-shirt-lolympique',
     href: '/archives/timeless-duck-shirt-lolympique',
   },
   {
     id: 'fausse-geometrie',
-    label: <Fragment>FW15: <b>[Fausse Geométrié]</b></Fragment>,
+    label: (
+      <Fragment>
+        FW15: <b>[Fausse Geométrié]</b>
+      </Fragment>
+    ),
     handle: 'fausse-geometrie',
     href: '/archives/fausse-geometrie',
   },
   {
     id: 'kapput',
-    label: <Fragment>SS16: <b>[Kaputt]</b></Fragment>,
+    label: (
+      <Fragment>
+        SS16: <b>[Kaputt]</b>
+      </Fragment>
+    ),
     handle: 'kapput',
     href: '/archives/kapput',
   },
   {
     id: 'singular-unity',
-    label: <Fragment>FW17: <b>[Singular Unity]</b></Fragment>,
+    label: (
+      <Fragment>
+        FW17: <b>[Singular Unity]</b>
+      </Fragment>
+    ),
     handle: 'singular-unity',
     href: '/archives/singular-unity',
   },
   {
     id: 'white-shirt-17-1',
-    label: <Fragment>17 White Shirt: <b>[Foregn Affair / Criminal Intent, 1st. Edition]</b></Fragment>,
+    label: (
+      <Fragment>
+        17 White Shirt: <b>[Foregn Affair / Criminal Intent, 1st. Edition]</b>
+      </Fragment>
+    ),
     handle: 'white-shirt-17-1',
     href: '/archives/white-shirt-17-1',
   },
   {
     id: 'white-shirt-17-2',
-    label: <Fragment>17 White Shirt: <b>[Identity Theft Cards, Bribery & Artnapping, 2nd. Edition]</b></Fragment>,
+    label: (
+      <Fragment>
+        17 White Shirt:{' '}
+        <b>[Identity Theft Cards, Bribery & Artnapping, 2nd. Edition]</b>
+      </Fragment>
+    ),
     handle: 'white-shirt-17-2',
     href: '/archives/white-shirt-17-2',
   },
   {
     id: 'white-shirt-18',
-    label: <Fragment>18 White Shirt: <b>[Murder Is My Business, 3rd. Edition]</b></Fragment>,
+    label: (
+      <Fragment>
+        18 White Shirt: <b>[Murder Is My Business, 3rd. Edition]</b>
+      </Fragment>
+    ),
     handle: 'white-shirt-18',
     href: '/archives/white-shirt-18',
   },
   {
     id: 'white-shirt-19',
-    label: <Fragment>19 White Shirt: <b>[Murder Is My Business II / El Pueblo Cuestiona, 4th. Edition]</b></Fragment>,
+    label: (
+      <Fragment>
+        19 White Shirt:{' '}
+        <b>[Murder Is My Business II / El Pueblo Cuestiona, 4th. Edition]</b>
+      </Fragment>
+    ),
     handle: 'white-shirt-19',
     href: '/archives/white-shirt-19',
+  },
+  {
+    id: 'a-necessary-risk',
+    label: (
+      <Fragment>
+        2022: <b>[A Necessary Risk]</b>
+      </Fragment>
+    ),
+    handle: 'a-necessary-risk',
+    href: '/archives/a-necessary-risk',
+  },
+  {
+    id: 'no-medium-inserted',
+    label: (
+      <Fragment>
+        FW22: <b>[No Medium Inserted]</b>
+      </Fragment>
+    ),
+    handle: 'no-medium-inserted',
+    href: '/archives/no-medium-inserted',
   },
 ]
 
