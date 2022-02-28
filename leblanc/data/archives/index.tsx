2ed94380620ca80f745fd3217cff3eed6b551707
Christian Diaz Portela <christiandp7@gmail.com>
2022-02-28 17:00:25 -0400

    Add Signular unity optimized

--- leblanc/data/archives/index.tsx
+++ leblanc/data/archives/index.tsx
@@ -6,6 +6,7 @@ import murderIsMyBusiness2 from './murder-is-my-business-2'
 import whiteShirt1 from './white-shirt-1'
 import fausseGeometrie from './fausse-geometrie'
 import valentinesAdCampaign from './valentines-ad-campaign'
+import singularUnity from './singular-unity'
 
 export type ArchiveImage = {
   src: string
@@ -179,23 +180,7 @@ export const archivesData: Archive[] = [
     shortText: 'Finalist at the Iberoamerican Design Biennial 2018',
     richText:
       'Singular Unity is based on the concept of technological singularity, or the way in which everything converges in one place due to the large amount of information that we are processing nowadays. We are highly connected and surrounded by information that we are constantly processing via our technological devices, resulting in a dilution or loss of cultural identity, leaving us all in a state of some form of “cut-and-paste”. Singular unity is made up of two conversations: the first is about the way in which technology affects us and is changing our perception of time, while the second focuses on class-based stereotypes in the Dominican Republic.',
-    images: [
-      {
-        src: '/assets/archives/singular-unity/singular-1.jpg',
-        width: 1785,
-        height: 2232,
-      },
-      {
-        src: '/assets/archives/singular-unity/singular-2.jpg',
-        width: 1953,
-        height: 2441,
-      },
-      {
-        src: '/assets/archives/singular-unity/singular-3.jpg',
-        width: 1841,
-        height: 2301,
-      },
-    ],
+    images: singularUnity,
     credits:
       'Photographed in the Dominican Republic by Fernando y Víctor. <br /> Modeled by Edwar Guerrero and Sheila Betances <br /> at the ISOTEX factory.',
     abbrSvg: 'FW17',
