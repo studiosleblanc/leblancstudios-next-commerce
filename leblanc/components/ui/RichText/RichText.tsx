6c2a88446511dd95a8923afa98db1981ff49cad6
Christian Diaz Portela <christiandp7@gmail.com>
2022-03-21 09:43:33 -0400

    Add uppercase style to RichText component

--- leblanc/components/ui/RichText/RichText.tsx
+++ leblanc/components/ui/RichText/RichText.tsx
@@ -1,12 +1,14 @@
 import React, { FC } from 'react'
+import cn from 'classnames'
 import s from './RichText.module.css'
 
 interface Props {
   children: React.ReactNode
+  uppercase: boolean
 }
 
-const RichText: FC<Props> = ({ children }) => {
-  return <div className={s.root}>{children}</div>
+const RichText: FC<Props> = ({ children, uppercase }) => {
+  return <div className={cn(s.root, { uppercase: uppercase })}>{children}</div>
 }
 
 export default RichText
