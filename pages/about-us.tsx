10d39d5913a4b1e508c8b77d206e73a86ef9caa1
Christian Diaz Portela <christiandp7@gmail.com>
2022-03-21 09:43:10 -0400

    Add about us view to about us page

--- pages/about-us.tsx
+++ pages/about-us.tsx
@@ -1,13 +1,13 @@
 import { getSearchStaticProps } from '@lib/search-props'
 import type { GetStaticPropsContext } from 'next'
 import { Layout } from '@leblanc/layouts'
-import { ComingSoon } from '@leblanc/components/ui'
+import { AboutUsView } from '@leblanc/views'
 
 export async function getStaticProps(context: GetStaticPropsContext) {
   return getSearchStaticProps(context)
 }
 export default function AboutUs() {
-  return <ComingSoon />
+  return <AboutUsView />
 }
 
 AboutUs.Layout = Layout
