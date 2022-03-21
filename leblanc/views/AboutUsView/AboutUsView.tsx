63724a28bf51f1bfc26f4a72712ec1c563b2e3eb
Christian Diaz Portela <christiandp7@gmail.com>
2022-03-21 09:42:41 -0400

    Add About us view and add content

---
+++ leblanc/views/AboutUsView/AboutUsView.tsx
@@ -0,0 +1,44 @@
+import { RichText } from '@leblanc/components/ui'
+import { MainLayout } from '@leblanc/layouts'
+import React from 'react'
+
+const PrivacyPolicyView = () => {
+  return (
+    <MainLayout>
+      <RichText uppercase>
+        <h1>About Us</h1>
+        <br />
+        <br />
+        <br />
+        LEBLANCSTUDIOS IS A DOMINICAN CLOTHING BRAND FOUNDED IN 2014 BY ANGELO BEATO
+        & YAMIL ARBAJE. WITH AN AWARENESS OF GLOBALIZATION, HIGHLIGHTING PERSONAL
+        IDENTITY AND THE BLEND OF RACES, LEBLANCSTUDIOS STRIVES TO BECOME A SOCIAL,
+        CULTURAL AND POLITICAL MOVEMENT THAT EMPOWERS A GENERATION OF YOUNG PEOPLE
+        ACROSS ALL CONTINENTS TO EXPRESS THEMSELVES.
+        <br />
+        <br />
+        THE BRAND AIMS TO REDEFINE A NEW CARIBBEAN MASCULINITY AND FEMININITY TO THE
+        FASHION LANDSCAPE.
+        <br />
+        <br />
+        Following the collection Singular Unity presented in the Dominican Republic
+        in 2017, LEBLANCSTUDIOS was a finalist at the Iberoamerican Design Biennial
+        in Madrid 2018. With the collection Murder is My Business, the brand was
+        invited to design a collection for the Memorial Museum of the Dominican
+        Resistance. Afterwards. In 2019 we did a TEDxYouth Talk in Santo Domingo,
+        where We discussed generating culture through fashion. That same year, Yamil
+        Arbaje was an award winner of the Fashion Scholarship Fund with his project
+        Calvin Klein Jeans x Dollar Tree. Later in 2020, Yamil’s graduate collection
+        work was selected among the best design portfolios by 1GRANARY x PINTEREST of
+        2020.
+        <br />
+        <br />
+        In addition our work has been featured in different local newspapers of the
+        Dominican Republic and internationally in Vogue Thailand, Office Magazine,
+        Models.com, No Kill Mag, and 1GRANARY.
+      </RichText>
+    </MainLayout>
+  )
+}
+
+export default PrivacyPolicyView
