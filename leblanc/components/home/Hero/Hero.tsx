990589df9011b0b463f4300a741d3590a48aa6a0
uriel rivero <urielrivps@gmail.com>
2023-03-20 21:22:22 -0300

    update

--- leblanc/components/home/Hero/Hero.tsx
+++ leblanc/components/home/Hero/Hero.tsx
@@ -3,24 +3,27 @@ import s from './Hero.module.css'
 import Image from 'next/image'
 import { Logo } from '@leblanc/svg'
 import { Navbar } from '@leblanc/components/home'
+import LogoDefault from '@leblanc/svg/logo-default'
 
 const Hero: FC = () => {
   return (
     <div className={s.root}>
-      <div className={s.heroContent}>
-        <Logo className={s.logo} />
-        <Navbar />
+      <div className={s.content}>
+        <LogoDefault />
+        <div className={s.nav}>
+          <Navbar />
+        </div>
       </div>
       <div className={s.imageContainer}>
         <Image
           priority
           unoptimized
-          src="/assets/home/banner.jpg"
+          src="/assets/home/hero.png"
           alt="Leblanc Studios"
           width={1920}
           height={1080}
           layout="responsive"
-          // objectFit="cover"
+          objectFit="cover"
           // layout="fill"
         />
       </div>
