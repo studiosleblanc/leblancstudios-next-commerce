86074787e0dd8ade0306d0ec7c6fcf58cddce935
Hubert R Leanos <leanoshubert@gmail.com>
2023-08-18 11:07:47 -0400

    uppercase and scattered between p

--- leblanc/views/GlossaryView/GlossaryView.tsx
+++ leblanc/views/GlossaryView/GlossaryView.tsx
@@ -5,31 +5,55 @@ import React from 'react'
 const GlossaryView = () => {
   return (
     <MainLayout>
-      <RichText centered>
+      <RichText centered uppercase>
         <h1>Glossary</h1>
         <br />
         <br />
         <br />
        <h2> Work Ethic</h2>
-LEBLANCSTUDIOS operates as an open, dynamic learning space before it operates as a business. 
-Its company structure works to dismantle traditional notions of hierarchy and places all of its studio members in complete control of their time, responsibilities, and relations. This open-ended plan allows for more streamlined access to financial, conceptual, and even emotional support for all involved. Hence, its members enjoy the freedom to develop their own projects while still remaining devoted to the development of a circularly growing enterprise.
-    <br />
-    <br />
+        LEBLANCSTUDIOS operates as an open, dynamic learning space before it operates as a business. 
+        Its company structure works to dismantle traditional notions of hierarchy and places all of its 
+        studio members in complete control of their time, responsibilities, and relations. This open-ended plan 
+        allows for more streamlined access to financial, conceptual, and even emotional support for all involved. 
+        Hence, its members enjoy the freedom to develop their own projects while still remaining devoted to the development 
+        of a circularly growing enterprise.
+        <br />
+        <br />
+        <br />
+        <br />
 
       <h2>Sustainability</h2>
-Reusing textiles, donating them amongst our sphere of designers and even repurposing fabrics from mediums outside of fashion are some of our many forays into sustainability. We take pride in sourcing the finest recycled textiles from all corners of the globe. Canada, Portugal, Peru, and China are just some of the spaces in which our trusted distributors focus on providing salvaged nylon, recycled polyester, and high-quality organic cotton to directly benefit our consumers.
+        Reusing textiles, donating them amongst our sphere of designers and even repurposing fabrics from 
+        mediums outside of fashion are some of our many forays into sustainability. We take pride in sourcing 
+        the finest recycled textiles from all corners of the globe. Canada, Portugal, Peru, and China are just some 
+        of the spaces in which our trusted distributors focus on providing salvaged nylon, recycled polyester, and high-quality 
+        organic cotton to directly benefit our consumers.
 
-Our local seamstresses, tailors, and craftsmen all operate under the highest levels of competence and renown. We consider their expansive sartorial knowledge indispensable, hence why their work is compensated accordingly. The factories we collaborate with, are held to the same level of importance and professional respect. We take extra care in ensuring that their practices account for proper water, textile, employee, and general resource administration that remain up to par with the highest international standards.
-    <br />
-    <br />
+        Our local seamstresses, tailors, and craftsmen all operate under the highest levels of competence and renown. 
+        We consider their expansive sartorial knowledge indispensable, hence why their work is compensated accordingly. The factories 
+        we collaborate with, are held to the same level of importance and professional respect. We take extra care in ensuring that their 
+        practices account for proper water, textile, employee, and general resource administration that remain up to par with the highest international standards.
+        <br />
+        <br />
+        <br />
+        <br />
 
       <h2>NYC/DR</h2>
-An active presence in both NYC and Santo Domingo has allowed us to translate our Caribbean sensibilities to an international audience. Yamil our Co-Founder and Production Manager, operates from NYC while Angelo Beato, our second founder and Creative Director, operates from the Dominican Republic. Yamil managed to complete his Bachelors's and MFA Degrees at Parsons School of Design while juggling a job at ODIN retail store and managing his duties at LEBLANCSTUDIOS while serving as a Production Manager for Connor McKnight.
-    <br />
-    <br />
+        An active presence in both NYC and Santo Domingo has allowed us to translate our Caribbean sensibilities to an international audience. 
+        Yamil our Co-Founder and Production Manager, operates from NYC while Angelo Beato, our second founder and Creative Director, operates from the Dominican Republic.
+         Yamil managed to complete his Bachelors's and MFA Degrees at Parsons School of Design while juggling a job at ODIN retail store and managing his duties at 
+         LEBLANCSTUDIOS while serving as a Production Manager for Connor McKnight.
+        <br />
+        <br />
+        <br />
+        <br />
 
     <h2>Collaboration</h2>
-Elevating the narrative, cultural and traditional codes of the global south is one of LEBLANCSTUDIOS many aesthetic goals. Thus, collaborations with some of its most vibrant creative voices are at the core of all our studio processes. Angolan illustrator Tiago Mena Abrantes, and Dominican painter Gustavo Pena, are just some of the names that adorn our sprawling list of collaborators. Their talents remain infused in our capsule collections, AD campaigns, standalone items, and graphical work further impacting the reach of our brand's ethos. Ultimately, we believe that no creative endeavor can be achieved without the collated efforts of a community. We strive to push our influence globally in the most ethical way we know how: by allowing the communities that inspire us, to directly help in designing the products that represent them.
+      Elevating the narrative, cultural and traditional codes of the global south is one of LEBLANCSTUDIOS many aesthetic goals. Thus, collaborations with some of its
+       most vibrant creative voices are at the core of all our studio processes. Angolan illustrator Tiago Mena Abrantes, and Dominican painter Gustavo Pena, are just 
+       some of the names that adorn our sprawling list of collaborators. Their talents remain infused in our capsule collections, AD campaigns, standalone items, and 
+       graphical work further impacting the reach of our brand's ethos. Ultimately, we believe that no creative endeavor can be achieved without the collated efforts of a community.
+        We strive to push our influence globally in the most ethical way we know how: by allowing the communities that inspire us, to directly help in designing the products that represent them.
 
       </RichText>
     </MainLayout>
