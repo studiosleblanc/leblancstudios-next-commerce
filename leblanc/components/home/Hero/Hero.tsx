d1c014710501a18b2d78dfc76c9a0ea37fb0ba80
Hubert R Leanos <leanoshubert@gmail.com>
2024-01-08 22:47:42 -0400

    add tag a and items style

--- leblanc/components/home/Hero/Hero.tsx
+++ leblanc/components/home/Hero/Hero.tsx
@@ -13,38 +13,76 @@ const Hero: FC = () => {
         <div className={s.column}>
           <div className={s.navContainer}>
             <h3 className={s.navTitle}>WHITE SHIRT</h3>
-            <ul>
-              <li>NEW ARRIVALS</li>
-              <li>GIFTS</li>
-              <li>JACKETS AND COATS</li>
-              <li>TOPS</li>
-              <li>BOTOMS</li>
-              <li>ACCESSORIES</li>
+            <ul className={s.navItems}>
+              <li>
+                <a href="#">NEW ARRIVALS</a>
+              </li>
+              <li>
+                <a href="#">GIFTS</a>
+              </li>
+              <li>
+                <a href="#">JACKETS AND COATS</a>
+              </li>
+              <li>
+                <a href="#">TOPS</a>
+              </li>
+              <li>
+                <a href="#">BOTOMS</a>
+              </li>
+              <li>
+                <a href="#">
+                  ACCESSORIES <a href="#"></a>
+                </a>
+              </li>
             </ul>
           </div>
         </div>
         <div className={s.column}>
           <div className={s.navContainer}>
             <h3 className={s.navTitle}>TIMELESS DUCK SHIRT</h3>
-            <ul>
-              <li>NEW ARRIVALS</li>
-              <li>GIFTS</li>
-              <li>JACKETS AND COATS</li>
-              <li>TOPS</li>
-              <li>BOTOMS</li>
-              <li>ACCESSORIES</li>
+            <ul className={s.navItems}>
+              <li>
+                <a href="#">NEW ARRIVALS</a>
+              </li>
+              <li>
+                <a href="#">GIFTS</a>
+              </li>
+              <li>
+                <a href="#">JACKETS AND COATS</a>
+              </li>
+              <li>
+                <a href="#">TOPS</a>
+              </li>
+              <li>
+                <a href="#">BOTOMS</a>
+              </li>
+              <li>
+                <a href="#">ACCESSORIES</a>
+              </li>
+              <br />
+              <li>
+                <a href="#">KIDS</a>
+              </li>
             </ul>
           </div>
         </div>
         <div className={s.column}>
           <div className={s.navContainer}>
             <h3 className={s.navTitle}>SPECIAL PROJECTS</h3>
-            <ul>
-              <li>STO-DGO</li>
-              <li>LBS + P.1991</li>
+            <ul className={s.navItems}>
+              <li>
+                <a href="#">STO-DGO</a>
+              </li>
+              <li>
+                <a href="#">LBS + P.1991</a>
+              </li>
               <br />
-              <li>THE PROWER OF SOUND</li>
-              <li>FILM + ART DIRECTION</li>
+              <li>
+                <a href="#">THE PROWER OF SOUND</a>
+              </li>
+              <li>
+                <a href="#">FILM + ART DIRECTION</a>
+              </li>
             </ul>
           </div>
         </div>
