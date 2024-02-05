6af6b59526e3221693234b5a53394720e2a95c45
Christian Diaz Portela <christiandp7@gmail.com>
2024-02-05 18:02:06 +0100

    Edit search icon

--- components/common/Searchbar/Searchbar.tsx
+++ components/common/Searchbar/Searchbar.tsx
@@ -2,6 +2,7 @@ import { FC, InputHTMLAttributes, useEffect, useMemo } from 'react'
 import cn from 'classnames'
 import s from './Searchbar.module.css'
 import { useRouter } from 'next/router'
+import SearchIcon from '@components/icons/SearchIcon'
 
 interface Props {
   className?: string
@@ -46,13 +47,7 @@ const Searchbar: FC<Props> = ({ className, id = 'search' }) => {
           onKeyUp={handleKeyUp}
         />
         <div className={s.iconContainer}>
-          <svg className={s.icon} fill="currentColor" viewBox="0 0 16 16">
-            <path
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-            />
-          </svg>
+          <SearchIcon />
         </div>
       </div>
     ),
---
+++ components/icons/SearchIcon.tsx
@@ -0,0 +1,13 @@
+interface Props {
+  size?: number
+}
+
+const SearchIcon = ({ size = 24 }: Props) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg"  fill="currentColor" width={size} height={size} viewBox="0 0 32 32">
+      <path fill="currentColor" fill-rule="evenodd" d="M13.46 24.45c-6.29 0-11.389-5.01-11.389-11.2 0-6.19 5.099-11.21 11.389-11.21 6.29 0 11.39 5.02 11.39 11.21 0 6.19-5.1 11.2-11.39 11.2Zm18.228 5.8-8.259-8.13c2.162-2.35 3.491-5.45 3.491-8.87C26.92 5.93 20.894 0 13.46 0 6.026 0 0 5.93 0 13.25c0 7.31 6.026 13.24 13.46 13.24a13.52 13.52 0 0 0 8.472-2.96l8.292 8.16c.405.4 1.06.4 1.464 0 .405-.39.405-1.04 0-1.44Z"/>
+    </svg>
+  )
+}
+
+export default SearchIcon
